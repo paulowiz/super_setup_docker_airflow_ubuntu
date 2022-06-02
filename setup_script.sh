dc04375ff6497cbf47bd48c65ac0b4cfc4451bb7
Paulo Mota <phmota@outlook.com.br>
2022-06-02 16:21:39 +0200

    included docker compose instalation

--- setup_script.sh
+++ setup_script.sh
@@ -1,3 +1,7 @@
+apt-get update -y
+apt-get install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 mkdir -p plugins 
 mkdir -p logs
 docker build . --tag extending_airflow:latest
